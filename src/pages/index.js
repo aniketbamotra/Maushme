878757642d79f7a5bed19217af51fa23029aa8c6
Aniket Bamotra <aniketbamotra.11@gmail.com>
2023-10-19 12:30:33 +0530

    added coming soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,28 @@ import heroVid from "../static/video/hero-vid.mp4";
 import AnimPanel1 from "../components/AnimPanel1";
 import Bestsellers from "../components/Bestsellers";
 import About from "../components/About";
+import lightLogo from "../images/light-logo.png"
 
 const IndexPage = () => {
   return (
     <main className="grid grid-cols-12">
-      <Topbar />
-      <div className="hero-section col-span-full relative mb-20">
-        <video src={heroVid} autoPlay loop muted className="relative -z-10" />
-        <h1 className="hero-text heading z-10 absolute top-28 px-36 left-1/2 -translate-x-2/4 text-6xl text-white text-center">
-          Discover our wide range of home décor products crafted with finest
-          materials.
+      {/* <Topbar /> */}
+      <div className="hero-section col-span-full relative">
+        <video src={heroVid} autoPlay loop muted className="relative -z-10 h-screen w-screen object-fill" />
+        <div className="z-10 absolute flex flex-col justify-center items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <img src={lightLogo} alt="logo" className=" " />
+        <h1 className="hero-text heading text-4xl text-white text-center">
+          Coming Soon!
         </h1>
+        </div>
       </div>
-      <h3 className=" col-start-4 col-end-10 px-10 heading text-3xl text-center mb-44">
+      {/* <h3 className=" col-start-4 col-end-10 px-10 heading text-3xl text-center mb-44">
         Crafted with love and perfection to give your home a touch of nature and
         relaxation.
       </h3>
       <AnimPanel1 />
       <Bestsellers />
-      <About />
+      <About /> */}
     </main>
   );
 };

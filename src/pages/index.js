550f40ab8d1e7d7ba13e926d0ac59c08588b767a
Aniket Bamotra <aniketbamotra.11@gmail.com>
2023-10-26 20:17:04 +0530

    static coming soon landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,12 @@ import Bestsellers from "../components/Bestsellers";
 import About from "../components/About";
 import lightLogo from "../images/light-logo.png"
 import comimgImg from "../images/coming.png"
+import staticBg from "../images/static-bg.jpg"
 
 const IndexPage = () => {
   return (
     <main className="grid grid-cols-12 overflow-hidden">
-      <Topbar />
+      {/* <Topbar />
       <div className="hero-section col-span-full relative h-screen mb-20">
         <video src={heroVid} autoPlay loop muted className="relative w-screen h-screen object-cover -z-10" />
         <h1 className="hero-text heading z-10 absolute top-1/4 px-0 2xl:px-36 left-1/2 -translate-x-2/4 text-2xl md:text-4xl xl:text-5xl text-white text-center">
@@ -24,7 +25,16 @@ const IndexPage = () => {
       </h3>
         <AnimPanel1 />
       <Bestsellers />
-      <About />
+      <About /> */}
+
+      <img src={staticBg} alt="" className="static-bg absolute w-screen h-screen blur-sm top-0 left-0 object-cover" />
+      <div className="bg-overlay w-screen h-screen bg-black absolute top-0 left-0 opacity-50"></div>
+      <div className="flex justify-center items-center relative top-0 left-0 w-screen h-screen">
+        <div className="static-content flex flex-col justify-center items-center gap-8">
+          <img src={lightLogo} className="logo-white w-60"></img>
+          <h1 className="heading font-bold text-white text-4xl text-center">COMING SOON</h1>
+        </div>
+      </div>
     </main>
   );
 };
